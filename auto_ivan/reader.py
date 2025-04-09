9d79c1ce894339c7578b938652f1c5d4bbb33652
iLusha80 <rastyfx@gmail.com>
2025-04-09 13:44:21 +0300

    fix: удалил 0 из табельников. уникальные табельники только

--- auto_ivan/reader.py
+++ auto_ivan/reader.py
@@ -17,15 +17,21 @@ class Reader:
         id_kp = mok_fetch_kp_id(cookies={}, code_kp=code_kp)
 
         bus_desc_url = self.main_data_sheet.rows[id_row].cells[1].value
-        employee_number_alone = self.main_data_sheet.rows[id_row].cells[3].value
+        try:
+            employee_number_alone = str(int(self.main_data_sheet.rows[id_row].cells[3].value))
+        except:
+            employee_number_alone = "0"
+
         tmp_employee_number_row = self.main_data_sheet.rows[id_row].cells[4].value
 
         employee_number_list = list()
         employee_number_list.append(employee_number_alone)
         employee_number_list.extend(self._get_employee_numbers(tmp_employee_number_row))
 
+        employee_number_set = set(employee_number_list)
+
         json_data = {
-            "assignees": [{"employeeNumber": num} for num in employee_number_list],
+            "assignees": [{"employeeNumber": num} for num in employee_number_set],
             "taskType": "TASK_BUS_DESC_CHECKBOX",
             "busDescCheckboxTaskRequest": {
                 "processId": id_kp,
@@ -35,9 +41,20 @@ class Reader:
         return json_data
 
     def _get_employee_numbers(self, tmp_employee_number_row: str) -> list:
+        """
+        Берет строку, разбивает ее на список, берет каждый элемент списка,
+        разбивает его на 2 части, берет вторую, конвертирует в int и добавляет
+        в результат
+        Args:
+            tmp_employee_number_row (str): строка, которую нужно разбить
+
+        Returns:
+            list: список employee_number
+        """
         lst_data = tmp_employee_number_row.split(';')
         result = list()
         for data in lst_data:
-            result.append(data.split('_')[1])
+            curr_empl = str(int(data.split('_')[1]))
+            result.append(curr_empl)
         return result
 
